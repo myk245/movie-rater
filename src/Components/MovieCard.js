329178016d392fa81f1ccc3f99ffac278eee2ba6
myk245 <myk245@nyu.edu>
2020-12-01 01:23:39 -0500

    add notes for movie show page

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -3,6 +3,7 @@ import React from 'react';
 const MovieCard = (props) => {
    return (
       <div className="movie-card">
+         {/* when a movie card is clicked, it brings you to the movie's show page with details -- fetch from the api using the movie's id (props.key)*/}
          <h1>{props.movie.title}</h1>
          <img
             src={props.movie.image}
