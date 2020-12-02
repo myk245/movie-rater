679f03c6d1e69efb8d31af63efc9bf5a5019d9f6
myk245 <myk245@nyu.edu>
2020-12-01 20:17:10 -0500

    switch MovieCard to class component

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,21 +1,24 @@
 import React from 'react'; 
 import '../Styles.css'
 
-const MovieCard = (props) => {
-   return (
-      <div className="movie-card">
-         {/* when a movie card is clicked, it brings you to the movie's show page with details -- fetch from the api using the movie's id (props.key)*/}
-         <p className="movie-title">{props.movie.title}</p>
-         <div className="img-container">
-            <img
-               className="movie-image"
-               src={props.movie.image}
-               alt={props.movie.title}
-            />
+class MovieCard extends React.Component{
+
+   render() {
+      return (
+         <div className="movie-card">
+            {/* when a movie card is clicked, it brings you to the movie's show page with details -- fetch from the api using the movie's id (props.key)*/}
+            <p className="movie-title">{this.props.movie.title}</p>
+            <div className="img-container">
+               <img
+                  className="movie-image"
+                  src={this.props.movie.image}
+                  alt={this.props.movie.title}
+               />
+            </div>
+            <button className="button">See Details</button>
          </div>
-         <a className="button">See Details</a>
-      </div>
-   )
+      )
+   }
 }
 
 export default MovieCard; 
